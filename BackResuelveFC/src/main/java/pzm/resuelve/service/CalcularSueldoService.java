456229a193b023746574ca2cad34bd0bbabf4ff0
Pablo Zuñiga <pablo.zuniga.mata@gmail.com>
2019-12-04 18:05:43 -0600

    Creamos el paquete service
    Creamos el paquete y la clase de nuestro servicio.

---
+++ BackResuelveFC/src/main/java/pzm/resuelve/service/CalcularSueldoService.java
@@ -0,0 +1,13 @@
+package pzm.resuelve.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Servicio que contienen los métodos para calcular los sueldos de los jugadores
+ *
+ * @author Pablo Z&uacute;niga Mata
+ * @since 04/12/2019
+ */
+public class CalcularSueldoService {
+
+}
